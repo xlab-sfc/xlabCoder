e53f2e2dc873f708d6b0ecfdd939d14a9b6c58a4
Hidemaro Fujinami <>
2018-05-24 01:41:46 +0900

    initial commit

--- 0001.py
+++ 0001.py
@@ -1,34 +1,15 @@
-n = 4
-a = [4,5,10,20]
+# n = input()
+
+a = [ int(x) for x in input().split(',')]
 
 max_len = 0
 
-triangles = []
+for i in range(len(a)):
+    for j in range(i+1, len(a)):
+        for k in range(j+1, len(a)):
+            if a[k] < a[i]+a[j]:
+                max = a[k] + a[i] + a[j]
+                if max_len < max:
+                    max_len = max
 
-def checktriangle(triangle):
-    result = None
-    if triangle[2] < triangle[0] + triangle[1]:
-        result = True
-    else:
-        result = False
-    return result
-
-def main():
-    triangle = [0, 0, 0]
-    for _i in range(len(a) - 2):
-        triangle[0] = a[_i]
-        for __i in range(len(a) - _i - 1):
-            triangle[1] = a[__i + _i + 1]
-            i = __i + _i + 1
-            for ___i in range(len(a) - i - 1):
-                triangle[2] = a[___i + __i + 1 + _i + 1]    
-                b = checktriangle(triangle)
-                if b:
-                    global max_len
-                    max_len = triangle[0] + triangle[1] + triangle[2]
-
-
-
-if __name__ == '__main__':
-    main()
-    print(max_len)
+print(max_len)
